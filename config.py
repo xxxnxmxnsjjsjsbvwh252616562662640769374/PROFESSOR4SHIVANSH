0695e92815713b12dfcbc6676391d3ea46f0538e
Why You Visit My Profile 😂 <153857610+xxxnxmxnsjjsjsbvwh252616562662640769374@users.noreply.github.com>
2024-08-28 13:50:25 +0530

    [ 𝐏𝐑𝚯𝐅𝐄𝐒𝐒𝚯𝐑 ][ ѕαиαтαиι ]

--- config.py
+++ config.py
@@ -33,15 +33,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/XFLSWAN/SHUKLA-MUSIC-V1",
+    "https://github.com/xxxnxmxnsjjsjsbvwh252616562662640769374/PROFESSOR4SHIVANSH",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Inflexupdate")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Inflex_Support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Professor_network")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Professorxnetwork")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "100"))
